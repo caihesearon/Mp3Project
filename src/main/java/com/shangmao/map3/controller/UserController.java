326c0400908b489198b68763f7467c8cad40f406
hecai <2435705112@qq.com>
2020-09-11 17:06:21 +0800

    解决Sonar报错

--- src/main/java/com/shangmao/map3/controller/UserController.java
+++ src/main/java/com/shangmao/map3/controller/UserController.java
@@ -4,7 +4,6 @@ import com.shangmao.map3.pojo.User;
 import com.shangmao.map3.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,8 +17,6 @@ public class UserController {
 
     @RequestMapping("/")
     public String index(){
-        List<User> allUser = iUserService.getAllUser();
-        System.out.println(allUser);
         return "index";
     }
 
