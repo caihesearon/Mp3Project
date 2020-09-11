aa77f7f028487be8462dd86fc7087185faa32b7e
hecai <2435705112@qq.com>
2020-09-11 17:04:58 +0800

    AJAX接通

--- src/main/java/com/shangmao/map3/controller/UserController.java
+++ src/main/java/com/shangmao/map3/controller/UserController.java
@@ -4,7 +4,9 @@ import com.shangmao.map3.pojo.User;
 import com.shangmao.map3.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -19,6 +21,12 @@ public class UserController {
         List<User> allUser = iUserService.getAllUser();
         System.out.println(allUser);
         return "index";
+    }
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public List<User> getJson(){
+        return iUserService.getAllUser();
     }
 
 
